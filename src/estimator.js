a5300fa0a171baaad6597c951dc9e3dd73459634
ayissi joel <ayissi.nke.joel@gmail.com>
2020-04-13 19:52:39 +0200

    firsr assigment

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => ({
+        data,
+
+        impact: {
+            currentlyInfected: data.reportedCases * 10 ,
+            infectionsByRequestedTime : impact.currentlyInfected *512
+        
+
+        }, 
+        severeImpact: {
+            currentlyInfected: data.reportedCases*50,
+            infectionsByRequestedTime : severeImpact.currentlyInfected * 512
+    
+        } 
+    } )
+
+
+     console.log("wtf");
 
 export default covid19ImpactEstimator;
